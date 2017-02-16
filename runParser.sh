3bf88d3fc87959c41f30aaf8894f3de56fde4f59
Colin M St. Claire <colin.stclaire@wsu.edu>
2017-02-16 14:35:57 -0800

    changed runParser.sh

--- runParser.sh
+++ runParser.sh
@@ -2,17 +2,11 @@
 # run python script to create item, user, category, and bid .dat files
 python parser.py ebay_data/items-*.xml
 
-# sort for removing duplicates
+# sort and removing duplicates
 sort -u item.dat -o item.dat
 sort -u user.dat -o user.dat
 sort -u category.dat -o category.dat
 sort -u bid.dat -o bid.dat
-
-# remove duplicates
-echo "$(uniq item.dat)" > item.dat
-echo "$(uniq user.dat)" > user.dat
-echo "$(uniq category.dat)" > category.dat
-echo "$(uniq bid.dat)" > bid.dat
 
 # create DB
 sqlite3 Auction.db < create.sql
