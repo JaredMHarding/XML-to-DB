3aa7e75ca26b0d1842eb64eb956ca6096ad0876a
jharding <jared.harding@wsu.edu>
2017-02-16 19:17:43 -0800

    Fixed query 5 It was including all users instead of just sellers

--- query5.sql
+++ query5.sql
@@ -1,5 +1,11 @@
 -- 5. Find the number of sellers whose rating is higher than 1000.
 -- should be 3130
-SELECT Count(UserId)
-FROM Users
-WHERE Rating > 1000;
+--SELECT Count(UserId)
+--FROM Users
+--WHERE Rating > 1000;
+select count(UserId)
+from Users
+where rating > 1000 and UserId in (
+  select SellerId
+  from Items
+);
