b66741fe387d1b553eeb6fabe5864fa44385f5d8
jharding <jared.harding@wsu.edu>
2017-02-16 19:36:27 -0800

    Fixed query 6 Have to make sure to use 'distinct' so it doesn't count users more than once

--- query6.sql
+++ query6.sql
@@ -1,6 +1,12 @@
-SELECT Count(SellerId)
-FROM Items, (
-     SELECT DISTINCT UserId
-     FROM Bids
-) as b
-WHERE SellerId = UserId.b;
+--SELECT Count(SellerId)
+--FROM Items, (
+--     SELECT DISTINCT UserId
+--     FROM Bids
+--) as b
+--WHERE SellerId = UserId.b;
+select count(distinct UserId)
+from Bids
+where UserId in (
+  select distinct SellerId
+  from Items
+);
