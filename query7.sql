5840d0c6d5b6ca584cb4f866369df3d45b1439a8
jharding <jared.harding@wsu.edu>
2017-02-16 19:59:26 -0800

    Fixed query 7. All of the queries listed in the project pdf work now

--- query7.sql
+++ query7.sql
@@ -11,13 +11,16 @@
 --) AS i
 --WHERE Categories.ItemId = i.itemId;
 
-SELECT Count(Category)
-FROM (
-  SELECT DISTINCT Category
-  FROM Categories, (
-    SELECT ItemId
-    FROM Items
-    WHERE CurrentBid > 100
-  ) AS I
-  WHERE Categories.ItemId = I.ItemId
-);
+--SELECT Count(Category)
+--FROM (
+--  SELECT DISTINCT Category
+--  FROM Categories, (
+--    SELECT ItemId
+--    FROM Items
+--    WHERE CurrentBid > 100
+--  ) AS I
+--  WHERE Categories.ItemId = I.ItemId
+--);
+select count(distinct Category)
+from Categories C, Bids B
+where C.ItemId = B.ItemId and B.Amount > 100;
