a0d2549a39b2b42f83b6ba0e358021fcc0a024ab
Colin M St. Claire <colin.stclaire@wsu.edu>
2017-02-17 12:38:58 -0800

    Goofed up merge :|

--- query7.sql
+++ query7.sql
@@ -1,9 +1,3 @@
---) as b
-6	--WHERE SellerId = UserId.b;
-6	--WHERE SellerId = UserId.b;
-7	select count(distinct UserId)
-7	select count(distinct UserId)
-8	from Bids
-8	from Bids B, Items I
-9	where UserId in (
-9	where B.UserId = I.SellerId;
+select count(distinct Category)
+from Categories C, Bids B
+where C.ItemId = B.ItemId and B.Amount > 100;
